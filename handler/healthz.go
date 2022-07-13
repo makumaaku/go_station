efa3eb0742b777f40364c53b4d8c929ee29a1ba1
MarkSaito <abokadoaoinaaiueo37@gmail.com>
2022-07-13 17:43:30 +0900

    sta8までクリア

---
+++ handler/healthz.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+	"log"
+	"encoding/json"
+
+	"github.com/TechBowl-japan/go-stations/model"
+)
+
+// A HealthzHandler implements health check endpoint.
+type HealthzHandler struct{}
+
+// NewHealthzHandler returns HealthzHandler based http.Handler.
+func NewHealthzHandler() *HealthzHandler {
+	return &HealthzHandler{}
+}
+
+// ServeHTTP implements http.Handler interface.
+func (h *HealthzHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	res := &model.HealthzResponse{}
+	res.Message = "OK"
+	err := json.NewEncoder(w).Encode(res)
+	
+	if err != nil {
+		log.Println(err)
+	}
+}
---
+++ service/todo.go
@@ -0,0 +1,68 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/TechBowl-japan/go-stations/model"
+	"github.com/mattn/go-sqlite3"
+)
+
+// A TODOService implements CRUD of TODO entities.
+type TODOService struct {
+	db *sql.DB
+}
+
+// NewTODOService returns new TODOService.
+func NewTODOService(db *sql.DB) *TODOService {
+	return &TODOService{
+		db: db,
+	}
+}
+
+// CreateTODO creates a TODO on DB.
+func (s *TODOService) CreateTODO(ctx context.Context, subject, description string) (*model.TODO, error) {
+	const (
+		insert  = `INSERT INTO todos(subject, description) VALUES(?, ?)`
+		confirm = `SELECT subject, description, created_at, updated_at FROM todos WHERE id = ?`
+	)
+	var todo model.TODO
+	if subject == "" {
+		return &todo, sqlite3.ErrConstraint
+	}
+
+	stmt, err := s.db.PrepareContext(ctx, insert)
+	result, err := stmt.ExecContext(ctx, subject, description)
+	todo.ID, err = result.LastInsertId()
+	row := s.db.QueryRowContext(ctx, confirm, todo.ID)
+	err = row.Scan(&todo.Subject, &todo.Description, &todo.CreatedAt, &todo.UpdatedAt)
+
+	return &todo, err
+}
+
+// ReadTODO reads TODOs on DB.
+func (s *TODOService) ReadTODO(ctx context.Context, prevID, size int64) ([]*model.TODO, error) {
+	const (
+		read       = `SELECT id, subject, description, created_at, updated_at FROM todos ORDER BY id DESC LIMIT ?`
+		readWithID = `SELECT id, subject, description, created_at, updated_at FROM todos WHERE id < ? ORDER BY id DESC LIMIT ?`
+	)
+
+	return nil, nil
+}
+
+// UpdateTODO updates the TODO on DB.
+func (s *TODOService) UpdateTODO(ctx context.Context, id int64, subject, description string) (*model.TODO, error) {
+	const (
+		update  = `UPDATE todos SET subject = ?, description = ? WHERE id = ?`
+		confirm = `SELECT subject, description, created_at, updated_at FROM todos WHERE id = ?`
+	)
+
+	return nil, nil
+}
+
+// DeleteTODO deletes TODOs on DB by ids.
+func (s *TODOService) DeleteTODO(ctx context.Context, ids []int64) error {
+	const deleteFmt = `DELETE FROM todos WHERE id IN (?%s)`
+
+	return nil
+}
